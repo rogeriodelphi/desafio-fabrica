cd8808cc79c319295d691401c908452f48cc6b21
rogeriodelphi <rogeriodelphi@gmail.com>
2022-07-20 23:32:07 -0300

    Configurando arquivos do heroku

--- desafio_fabrica/settings.py
+++ desafio_fabrica/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -73,6 +74,9 @@ DATABASES = {
     }
 }
 
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
